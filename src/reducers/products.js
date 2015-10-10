7cef1197883920356b6a1093175c4a2c6a5ba5ef
Julien Rodrigues <contact@jrodrigues.me>
2015-10-10 12:13:51 -0400

    wip

---
+++ src/reducers/products.js
@@ -0,0 +1,24 @@
+import {ADD_PRODUCT} from '../actions/products';
+
+const INITIAL_STATE = [{
+  name: 'Bananas'
+}];
+
+
+/**
+ * Products reducer.
+ * @method products
+ * @param {array} state - The current state.
+ * @param {string} action - The action to perform.
+ * @returns {object} Updated state
+ */
+export default function(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case ADD_PRODUCT:
+    return state.concat({
+      name: action.name
+    });
+  default:
+    return state;
+  }
+}
